8de944f46c1b067c666a5fcfd7dd8a250c1db780
bashirmurtaza93 <bashirmurtaza93@gmail.com>
2017-06-27 15:39:16 -0400

    Started styling up the page a bit

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -6,55 +6,62 @@ $(document).ready(function(){
 
 
 function searchInfluencer(){
-
-  $(document).on('click','#searchButton',function(){
-    var data = $('#influencerSearch').val();
-
-    var async1 = $.ajax({
-      type: 'POST',
-      url:'/instagram',
-      data:{
-        influencer: data
-      },
-      success: function(response){
-        influencerObj.instagram = response;
-      }
-    });
-    var async2 = $.ajax({
-      type: 'POST',
-      url:'/facebook',
-      data:{
-        influencer: data
-      },
-      success: function(response){
-        influencerObj.facebook = response;
-      }
-    });
-    var async3 = $.ajax({
-      type: 'POST',
-      url:'/twitter',
-      data:{
-        influencer: data
-      },
-      success: function(response){
-        influencerObj.twitter = response;
-
-      }
-    });
-    $.when(async3,async2,async1).done(function(){
-      renderPlatforms(influencerObj);
-    })
+  $(document).on('click','#search-button',function(){
+    search();
+  })
+  $(document).on('keyup','#influencer-search',function(e){
+    if(e.which == 13){
+      search();
+    }
   })
 
-
 }
+
+
+function search(){
+  var data = $('#influencer-search').val();
+  var async1 = $.ajax({
+    type: 'POST',
+    url:'/instagram',
+    data:{
+      influencer: data
+    },
+    success: function(response){
+      influencerObj.instagram = response;
+    }
+  });
+  var async2 = $.ajax({
+    type: 'POST',
+    url:'/facebook',
+    data:{
+      influencer: data
+    },
+    success: function(response){
+      influencerObj.facebook = response;
+    }
+  });
+  var async3 = $.ajax({
+    type: 'POST',
+    url:'/twitter',
+    data:{
+      influencer: data
+    },
+    success: function(response){
+      influencerObj.twitter = response;
+    }
+  });
+  $.when(async3,async2,async1).done(function(){
+    renderPlatforms(influencerObj);
+  })
+}
+
 
 function renderPlatforms(influencerObj){
 
   $.each(influencerObj,function(key,json){
-    $('.'+key+'Name').text(json.full_name);
-    $('.'+key+'Followers').text(json.followers);
-    $('.'+key+'Photo').attr('src',json.image);
+    $('.'+key+'-name').text(json.full_name);
+    $('.'+key+'-followers').text(json.followers);
+    $('.'+key+'-photo').attr('src',json.image);
   });
 
 }
